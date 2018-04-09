2011a7612573f9f2015bd0c040ce13586bcdce19
Kacper Kowalski <kacper.kowalski@novamedia.pl>
2018-04-09 14:05:23 +0200

    checker: return immediately when array is NULL

--- array.c
+++ array.c
@@ -7,6 +7,9 @@
 
 void *array_search(void *arr, size_t elem_size, size_t len, arr_search_checker v, void *match)
 {
+	if(!arr)
+		return NULL;
+
 	char *p = arr;
 	for(unsigned i = 0; i < len; ++i)
 	{
